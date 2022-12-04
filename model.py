4629a6db57fb942b0d920abd17ac1206313c718e
Captain525 <dheffren@rocketmail.com>
2022-12-04 12:36:26 -0500

    added a tokenizer functionality, reverse order sentences (weird characteristics though

--- model.py
+++ model.py
@@ -1,17 +1,51 @@
 #look at this website https://huggingface.co/docs/transformers/v4.24.0/en/model_doc/gpt2#overview
 import tensorflow as tf
-from preprocess import get_data
+from preprocess import *
 from collectData import collectDataFromTextDoc
 from transformers import TFGPT2Model, GPT2Tokenizer
+import random
 vocabSize = 2048
 embedSize = 1000
-textCorpus = get_data("data/")
-print(len(textCorpus[0]))
+textCorpus = get_data_poems("data/")
+#sample a line from the dataset? 
+sample = random.sample(textCorpus, 1)[0]
+print(sample)
+mergedSample = merge_lines(sample, use_bos = True, order = None)
+print(mergedSample)
+
+#print(textCorpus)
+print(len(textCorpus))
 print("before tokenizer")
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-tokenizedText = tokenizer(textCorpus, return_length = True)
-print(tokenizedText.length)
-#print(tokenizedText[0])
+special_tokens = {
+    "sep_token": "<LINE>",
+    "pad_token": "<PAD>", 
+    "bos_token": "<BOS>", 
+    "eos_token": "<EOS>"
+
+}
+tokenizer.add_special_tokens(special_tokens)
+tokenizedText = tokenizer(mergedSample)['input_ids']
+untokenized  = tokenizer.decode(tokenizedText)
+
+detokenizePart = tokenizer.decode(tokenizedText[0:2])
+
+
+#print(untokenized)
+#print(tokenizedText)
+
+#print(tokenizedText)
+#linesReversed = reverseLineOrder(mergedSample.split(" "), use_bos = True, tokenizer = tokenizer)
+#print(linesReversed)
+print("tokenized length: ", len(tokenizedText))
+input_ids = list(reverseLineOrder(tokenizedText, use_bos = True, tokenizer = tokenizer, reverse_last_line = True))
+#print(input_ids)
+print("reversed length: ", len(input_ids))
+detokenizePartOfInput = tokenizer.decode(tokenizedText[0:2])
+print("detokenized: ", detokenizePartOfInput)
+decodeString = tokenizer.decode(input_ids, clean_up_tokenization_spaces = True)
+
+print(decodeString)
 #configuration = GPT2Config(vocab_size = vocabSize, n_positions = 2048, n_embd= embedSize, n_layer = 12, n_head = 12, n_inner = None, activation_function = "relu")
 
 #model with weights
--- preprocess.py
+++ preprocess.py
@@ -3,6 +3,9 @@ import os
 import string
 
 def get_data(path='../data'):
+    """
+    Gets a list of strings, where each string is a word in a line. 
+    """
     
     poem_lst = []
     files = os.listdir(path)
@@ -22,3 +25,140 @@ def get_data(path='../data'):
         output_lst.append(split_poem)
 
     return output_lst
+
+def get_data_lines(path = '../data'):
+    """
+    Gets a list of lines, where each line is a string. 
+    """
+    poem_lst = []
+    files = os.listdir(path)
+    for file in files: # read in all poems
+        poem_path = os.path.join(path, file)
+        with open(poem_path, 'r') as poem:
+            poem_text = poem.readlines()
+        poem_lst.append(poem_text)
+    
+    output_lst = []
+    for poem in poem_lst:
+       
+        poem[0] = "<BOS> " + poem[0]
+        poem[-1]= poem[-1] + " <EOS>"
+        for line in poem:
+            line = line.replace("\n", "").lower()
+            #print(repr(line))
+            if line == "":
+                continue
+            if("\n" in line):
+                assert(1==0)
+            output_lst.append(line)
+    return output_lst
+def get_data_poems(path = '.../data'):
+    """
+    Gets data as a list of poems, where each poem is 
+    a  list of lines, where each line is a string. 
+    """
+    poem_lst = []
+    files = os.listdir(path)
+    for file in files: # read in all poems
+        poem_path = os.path.join(path, file)
+        with open(poem_path, 'r') as poem:
+            poem_text = poem.readlines()
+        poem_lst.append(poem_text)
+    
+    output_lst = []
+    for poem in poem_lst:
+        lineList = []
+        #will add BOS and EOS later. 
+        for id, line in enumerate(poem):
+            line = line.replace("\n", "").lower()
+            #print(repr(line))
+            if line == "":
+                continue
+            #gets rid of ending punctuation in line. 
+            if line[-1] in string.punctuation:
+                line = line[:-1]
+            assert("\n" not in line)
+            lineList.append(line)
+        output_lst.append(lineList)
+    return output_lst
+
+def merge_lines(lines, use_bos, order = None):
+    """
+    Input is a list of "line" strings. 
+    Output makes it one string but adds line delimiters. 
+    """
+    #reorders the lines based on the input list/ordering. 
+    if order is not None: 
+        try: 
+            order = list(order)
+        except Exception:
+            return
+        assert isinstance(order, list)
+        assert(sorted(order) == list(range(len(order))))
+        lines = [lines[0] for o in order]
+    #merges the lines into one. 
+    words = ' <LINE> '.join(lines) + ' <LINE>'
+    #adds bos if we need to. 
+    if(use_bos):
+        words = '<BOS> ' + words + ' <EOS>'
+     
+    words = ' '.join(words.split())
+    return words
+def reorder(lines, order = None):
+    if order is None:
+        return lines
+    else: 
+        new = [(o,i) for i,o in enumerate(order)]
+        #sort by o not by i. the value of o which is 0 is in the first index now, and its coupled with teh i it corresponds to. 
+        new = sorted(new)
+        #get the enumerate values from the sorted list. These represent the index of the input which should be in each spot. 
+        new = [o[1] for o in new]
+        #if new[0] = 1, that means the 2nd line of lines had the lowest element of the order, which means that it should be first in the output. 
+        #switch order of lines based on those enumerate values. 
+        lines = [lines[o] for o in new]
+    return lines
+def reverseLineOrder(input_ids, use_bos, tokenizer, reverse_last_line = False):
+    pad_token_id = tokenizer.pad_token_id
+    print(pad_token_id)
+    start = 0
+    #basically get the starting point of the sequence without padding. 
+    for i, id_ in enumerate(input_ids):
+        if id_ !=pad_token_id:
+            init, start = i,i
+            break
+    tmp_input_ids = input_ids[start:]
+    new_input_ids = np.zeros_like(tmp_input_ids, dtype = np.int32)
+
+    if use_bos:
+        #transmit the BOS string to the start of the reverse order sentence. 
+        new_input_ids[0] = tmp_input_ids[0]
+        start = 1
+
+    else:
+        start = 0
+    print("sep token: ", tokenizer.sep_token_id)
+    for end in range(1, len(tmp_input_ids)):
+        
+        #iterate through sentence until you reach a sep token. 
+        if tmp_input_ids[end] == tokenizer.sep_token_id:
+            #make all the ids in that area reversed, ie you reverse the line. 
+            #print(tmp_input_ids[start:end])
+            new_input_ids[start:end] = tmp_input_ids[start:end][::-1]
+            #add the sep token at the end. 
+            new_input_ids[end] = tokenizer.sep_token_id
+            #move on to later words/phrases. 
+        
+            #print(new_input_ids[start:end+1])
+            #print(new_input_ids[start:end+1].shape)
+            start = end + 1
+    #shoudl end up with start at the beginning of the last line.
+    # If we want to reverse it, we can reverse entire ending from start to end.  
+    if reverse_last_line:
+        #new input ids from start to hte end
+        new_input_ids[start:-1] = tmp_input_ids[start:-1][::-1]
+        #EOS CHARACTER. NOT IN ORIGINAL CODE, SO MAY BE WRONG. 
+        new_input_ids[-1] = tmp_input_ids[-1]
+    else:
+        new_input_ids[start:] = tmp_input_ids[start:]
+    new_input_ids = np.concatenate([input_ids[:init], new_input_ids], axis=0)
+    return new_input_ids.astype(np.int32)
